75d2c477524a95fdb0f024ee1273e49717e86537
Kyle Van Berendonck <kyle@vanberendonck.com>
2014-10-01 21:12:32 +1000

    Add rot13 for ByteString and split code out into reusable bits.

--- src/Codec/Rot13.hs
+++ src/Codec/Rot13.hs
@@ -1,22 +1,35 @@
 
 module Codec.Rot13
-  ( rot13char
+  ( rot13word
+  , rot13char
   , rot13
+  , rot13bs
   ) where
 
-import Data.Char
-import Data.Word
+import           Data.Char
+import           Data.Word
+import qualified Data.ByteString as BS
+
+
+{-# INLINE rot13word #-}
+rot13word :: Word -> Word
+rot13word x
+  | x - 97 < 26 = 97 + rem (x - 84) 26
+  | x - 65 < 26 = 65 + rem (x - 52) 26
+  | otherwise   = x
+
+{-# INLINE rot13word8 #-}
+rot13word8 :: Word8 -> Word8
+rot13word8 = \x -> (fromIntegral . rot13word . fromIntegral) x
 
 {-# INLINE rot13char #-}
 rot13char :: Char -> Char
-rot13char = \x -> (chr . fromIntegral . transform . fromIntegral . ord) x
-  where
-    transform :: Word -> Word
-    transform x
-      | x - 97 < 26 = 97 + rem (x - 84) 26
-      | x - 65 < 26 = 65 + rem (x - 52) 26
-      | otherwise   = x
+rot13char = \x -> (chr . fromIntegral . rot13word . fromIntegral . ord) x
 
-{-# INLINE rot13 #-}
+{-# INLINABLE rot13bs #-}
+rot13bs :: BS.ByteString -> BS.ByteString
+rot13bs = \s -> BS.map rot13word8 s
+
+{-# INLINABLE rot13 #-}
 rot13 :: String -> String
 rot13 = \s -> map rot13char s
